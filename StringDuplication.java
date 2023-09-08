063e3463fa51def029b4ad38b9547e2976f6a739
VensSent <venssent@mail.ru>
2023-09-08 14:28:29 +0300

    String Duplication

--- StringDuplication.java
+++ StringDuplication.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class StringDuplication {
+    public static void main(String[] args) {
+        String s;
+        int n;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter your string to repeat n times");
+        s = input.nextLine();
+        System.out.println("Enter number of times to repeat");
+        n = input.nextInt();
+
+        printNtimes(s,n);
+
+
+    }
+
+    public static void printNtimes(String strIn, int numbIn){
+        for (int i = 0; i<numbIn; i++){
+            System.out.println(strIn);
+        }
+    }
 }
