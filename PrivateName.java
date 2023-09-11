28f0fae7fbc5c2fff9b486bc832326aa05563339
Ivan <48672516+VensSent@users.noreply.github.com>
2023-09-11 10:42:39 +0300

    Create PrivateName.java

---
+++ PrivateName.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String name;
+ 
+    public void setName(String name) {
+        this.name = name;
+    }
+    public static void main(String[] args) {
+      Cat myCat = new Cat();
+      myCat.setName("QWERTY");
+    }
+}
