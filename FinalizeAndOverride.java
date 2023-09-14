50cefcf292160a3974530bde3067a6756c187e29
Ivan <48672516+VensSent@users.noreply.github.com>
2023-09-14 15:58:21 +0300

    Create FinalizeAndOverride.java

---
+++ FinalizeAndOverride.java
@@ -0,0 +1,33 @@
+/*
+
+1.По 50 000 объектов Cat и Dog
+
+Создать в цикле по 50 000 объектов Cat и Dog. 
+(Java-машина должна начать уничтожать неиспользуемые, и метод finalize хоть раз да вызовется).
+
+*/
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        for (int i = 0; i <50000 ; i++) {
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+        }
+    }
+}
+class Cat{
+    @Override
+    protected void finalize() throws Throwable{
+        super.finalize();
+        System.out.println("Cat destroyed");
+    }
+}
+
+class Dog{
+    @Override
+    protected void finalize() throws Throwable{
+        super.finalize();
+        System.out.println("Dog destroyed");
+    }
+}
